398a7e12f5e5a40862e8920994f4cbb4bb0e374e
Tiffany Hays <youfoundtiffany@gmail.com>
2023-08-14 16:18:47 -0600

    almost done

--- script.js
+++ script.js
@@ -1,5 +1,74 @@
-console.log("Hello World")
 
-function reset() {
-    console.log("reset")
+
+// Game Data
+let home = 0
+let away = 0
+
+// Interaction with DOM, alias for document.getElementById
+let homeScoreElm = document.getElementById('homeScore')
+let awayScoreElm = document.getElementById('awayScore')
+
+
+// HOME POINTS
+// PLUS ONE
+function homePointsOne() {
+    console.log('home +1')
+    home += 1
+    // let elem = document.getElementById('homeScore')
+    homeScoreElm.innerText = home
 }
+// PLUS THREE
+function homePointsThree() {
+    console.log('home +3')
+    home += 3
+    // let elem = document.getElementById('homeScore')
+    homeScoreElm.innerText = home
+    maxScore()
+}
+
+// AWAY POINTS
+// PLUS ONE
+function awayPointsOne() {
+    console.log('away +1')
+    away += 1
+    // let elem = document.getElementById('awayScore')
+    awayScoreElm.innerText = away
+    maxScore()
+}
+// PLUS THREE
+function awayPointsThree() {
+    console.log('away +3')
+    away += 3
+    // let elem = document.getElementById('awayScore')
+    awayScoreElm.innerText = away
+    maxScore()
+}
+
+// RESET BUTTON
+function reset() {
+    console.log("new game")
+    home = 0
+    away = 0
+    // let elem = document.getElementById('homeScore')
+    homeScoreElm.innerText = home
+    // let elem = document.getElementById('awa7Score')
+    awayScoreElm.innerText = home
+}
+
+// MAX SCORE
+function maxScore() {
+    if (home >= 10) {
+        console.log("home wins")
+        alert("Home Wins!")
+        reset()
+    } else if (away >= 10) {
+        console.log("away wins")
+        alert("Away Wins!")
+        reset()
+    }
+}
+
+// DRAW FUNCTION, SCORING
+// function scoring {
+
+// }
